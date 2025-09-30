da0bf007aade9aab7e1acb08669f7faf01fc9594
Baljeet Singh <baljeet.matta@gmail.com>
2025-09-30 11:46:23 +0530

    Login Signup & Routing

--- eserver.js
+++ eserver.js
@@ -1,24 +1,31 @@
 const express=require("express");
 const path=require("path");
+const fs=require("fs");
+const userRoutes=require("./routes/userRoutes");
+
 
 const app=express();//createServer->callback function
 app.use(express.static("."));
 //bodyparser
 // express.urlEncoded
 // bodyparser
+
 app.use(express.urlencoded({extended:false}));
+app.use("/users",userRoutes);
+
+
 //extended ->false , querystring-> simple key/value 
 //extended>true,   qs-> rich json libray
 
-app.get("/save",(req,res)=>{
-    console.log(req.query);
-    res.send("Welcome "+req.query.username);
+// app.get("/save",(req,res)=>{
+//     console.log(req.query);
+//     res.send("Welcome "+req.query.username);
 
-})
-app.post("/save",(req,res)=>{
-      console.log(req.body);
-    res.send("Welcome "+req.body.username);
-})
+// })
+// app.post("/save",(req,res)=>{
+//       console.log(req.body);
+//     res.send("Welcome "+req.body.username);
+// })
 
 
 
@@ -44,4 +51,12 @@ app.post("/save",(req,res)=>{
 
 //     res.sendStatus(404).send("Page not found")
 // })
+
+app.get("/auth/dashboard",(req,res)=>{
+    res.send("Dashboard page")
+})
+
+
+
 app.listen(3000);
+
---
+++ routes/userRoutes.js
@@ -0,0 +1,68 @@
+const express=require("express");
+const path=require("path");
+const routes=express.Router();
+const fs=require("fs");
+
+routes.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../login.html"));
+})
+routes.get("/signup",(req,res)=>{
+ res.sendFile(path.join(__dirname,"../signup.html"));
+})
+routes.post("/login",(req,res)=>{
+
+     fs.readFile("./users.json","utf-8",(err,data)=>{
+
+        let users=JSON.parse(data);
+          const results= users.filter((item)=>{
+                    if(item.username==req.body.username)
+                        return true;
+                });
+        if(results.length==0)
+            res.redirect("/Login.html");
+        else
+            res.redirect("/auth/dashboard");
+
+
+     });
+
+})
+routes.post("/register",(req,res)=>{
+    
+        let obj={
+            username:req.body.username,
+            password:req.body.password
+        }
+        let users=[];
+        console.log(req.body.username)
+;        fs.readFile("./users.json","utf-8",(err,data)=>{
+
+            if(err)
+                users=[];
+            else
+            {
+                users=JSON.parse(data);
+                console.log(users);
+               const results= users.filter((item)=>{
+                    if(item.username==req.body.username)
+                        return true;
+                });
+                console.log(results);
+
+            
+                if(results.length!=0)
+                    res.send("User already exists");
+                else
+                {
+                    users.push(obj);
+                    fs.writeFile("./users.json",JSON.stringify(users),()=>{
+                        res.send("User created successfully");
+
+                    })
+                }
+            }
+        })
+
+})
+
+module.exports=routes;
